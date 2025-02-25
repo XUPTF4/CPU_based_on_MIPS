83efd9492906493ca312e318b46132d3e2f91a79
L33Z22L11 <L33Z22L11@gmail.com>
2025-02-25 11:15:37 +0800

    revert: Helpers.v

--- CPU/Helpers.v
+++ CPU/Helpers.v
@@ -2,12 +2,12 @@
 
 `ifndef HELPERS_V // 防止重复包含的宏定义
 `define HELPERS_V
-        // 符号扩展宏：signExtend(num, n)
+// 符号扩展宏：signExtend(num, n)
 `define signExtend(num, n) {{(32 - n){num[n - 1]}}, num}
-        // 零扩展宏：zeroExtend(num, n)
+// 零扩展宏：zeroExtend(num, n)
 `define zeroExtend(num, n) {{(32 - n){1'b0}}, num}
 
-        parameter ALU_X    = 6'b000000;
+parameter ALU_X    = 6'b000000;
 parameter ALU_ADD  = 6'b000001;
 parameter ALU_SUB  = 6'b000010;
 parameter ALU_AND  = 6'b000011;
